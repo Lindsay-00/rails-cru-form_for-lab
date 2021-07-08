c7e9ce0f0c7f29d87a91ba2f40b72edcbde368ab
Lindsay <lindsay.chen@yale.edu>
2021-07-09 05:38:13 +0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :artists
+  resources :genres
+  post "/artists/new", to: "artists#create"
+  post "/artists/:id/edit", to: "artists#update"
+  post "/genres/new", to: "genres#create"
+  post "/genres/:id/edit", to: "genres#update"
+  post "/songs/new", to: "songs#create"
+  post "/songs/:id/edit", to: "songs#update"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
